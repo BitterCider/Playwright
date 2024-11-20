57237a9dc940c8310fb69a3d6e41ea5fc071403e
Tal Baruchi <talbaruchy@gmail.com>
2024-11-20 11:00:02 +0200

    לבדיקה

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,19 +1,9 @@
-// @ts-check
-const { test, expect } = require('@playwright/test');
+  import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
-
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+test('test', async ({ page }) => {
+  await page.goto('https://www.google.com/');
+  await page.getByLabel('חיפוש', { exact: true }).fill('playwright');
+  await page.goto('https://www.google.com/search?q=playwright&sca_esv=77c72257878c30ef&source=hp&ei=mr88Z5XTOsWgnesPkNvM6Ao&iflsig=AL9hbdgAAAAAZzzNqtZKSZOCTeeO56FHuRahWLfEh889&ved=0ahUKEwiViKmV6-iJAxVFUGcHHZAtE60Q4dUDCA0&uact=5&oq=playwright&gs_lp=Egdnd3Mtd2l6IgpwbGF5d3JpZ2h0MgsQABiABBixAxiDATIFEAAYgAQyBRAAGIAEMgUQABiABDIFEAAYgAQyBRAAGIAEMgUQABiABDIFEAAYgAQyBRAAGIAEMgUQABiABEiWKVCYB1iOJXAFeACQAQCYAWKgAcMIqgECMTK4AQPIAQD4AQGYAhGgAusIqAIKwgIKEC4YAxjqAhiPAcICChAAGAMY6gIYjwHCAg0QLhgDGNQCGOoCGI8BwgIREC4YgAQYsQMY0QMYgwEYxwHCAg4QLhiABBixAxjRAxjHAcICCxAuGIAEGNEDGMcBwgIFEC4YgATCAggQABiABBixA8ICDRAAGIAEGLEDGIMBGArCAgcQABiABBgKmAMGkgcEMTYuMaAHy1M&sclient=gws-wiz');
+  await page.getByRole('link', { name: 'Playwright: Fast and reliable' }).click();
+  await expect(page.getByRole('heading', { name: 'Playwright enables reliable' }).locator('span')).toBeVisible();
 });
--- tests/firstTestCase.spec.js
+++ tests/firstTestCase.spec.js
@@ -1,4 +1,92 @@
-import { expect, test } from '@playwright/test';
+// ***npx playwright codegen***
+
+import { expect, test } from "@playwright/test";
+const testcafeURL = "https://devexpress.github.io/testcafe/example/";
 
 
+test.beforeEach(async ({page}) => {
+  await page.goto(testcafeURL)
+})
 
+
+//1
+test.describe("Checkboxes validation", () => {
+  test("3 Checkboxes", async ({ page }) => {
+    await page.getByTestId("remote-testing-checkbox").check();
+    await page.getByTestId("reusing-js-code-checkbox").check();
+    await page.getByTestId("parallel-testing-checkbox").check();
+    await expect(page.getByTestId("remote-testing-checkbox")).toBeChecked();
+  });
+});
+
+//2
+test.describe("radio button validation", () => {
+  test("OS radio button - windwos", async ({ page }) => {
+    await page.getByTestId("windows-radio").check();
+    await expect(page.getByTestId("windows-radio")).toBeChecked();
+  });
+});
+
+//3
+test.describe("checklist validation", () => {
+  test("JavaScriptAPI", async ({ page }) => {
+    await page.getByTestId("preferred-interface-select").selectOption("JavaScript API");
+    await expect(page.getByTestId("preferred-interface-select")).toHaveValue("JavaScript API");
+  });
+});
+
+//4
+test.describe.only("slider validation", () => {
+  test("isDisabled", async ({ page }) => {
+    await expect(page.locator("#tried-test-cafe")).not.toBeChecked();
+    await expect(page.locator("#slider")).toHaveClass(/ui-slider-disabled/);
+  });
+
+//5
+  test("isEnabled", async ({ page }) => {
+    await page.locator('#tried-test-cafe').click();
+    await expect(page.locator("#tried-test-cafe")).toBeChecked();
+    await expect(page.locator("#slider")).toBeEnabled();
+  });
+});
+
+//6
+test("isEnabled", async ({ page }) => {
+  await page.locator('#tried-test-cafe').click();
+  await page.locator('#comments').click()
+  await page.locator('#comments').fill('I am doing Automation\nIm an automated comment\nI should assert this works');
+  await expect(page.locator('#comments')).toHaveValue('I am doing Automation\nIm an automated comment\nI should assert this works')
+});
+
+//7
+test("submitIsDisabled", async ({page}) =>{
+  await expect(page.locator("#submit-button")).toHaveAttribute("disabled")
+});
+
+//8
+test('fillForm', async ({ page }) => {
+  await page.getByTestId('remote-testing-checkbox').check();
+  await page.getByTestId('analysis-checkbox').check();
+  await page.getByTestId('windows-radio').check();
+  await page.getByTestId('tried-testcafe-checkbox').check();
+  await page.getByTestId('name-input').click();
+  await page.getByTestId('name-input').type('Peter Parker');
+  await page.getByTestId('comments-area').click();
+  await page.getByTestId('comments-area').type('lol');
+  await page.getByTestId('submit-button').click();
+  await expect(page.getByTestId('thank-you-header')).toContainText('Thank you, Peter Parker!');
+});
+
+//9
+test('fillForm&URL', async ({ page }) => {
+  await page.getByTestId('remote-testing-checkbox').check();
+  await page.getByTestId('analysis-checkbox').check();
+  await page.getByTestId('windows-radio').check();
+  await page.getByTestId('tried-testcafe-checkbox').check();
+  await page.getByTestId('name-input').click();
+  await page.getByTestId('name-input').type('Peter Parker');
+  await page.getByTestId('comments-area').click();
+  await page.getByTestId('comments-area').type('lol');
+  await page.getByTestId('submit-button').click();
+  await expect(page).toHaveURL('https://devexpress.github.io/testcafe/example/thank-you.html')
+});
